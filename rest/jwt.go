5a20aa80d7836cce350e4fc04f271be4eb54470d
unknown <weeco91@gmail.com>
2020-01-01 15:57:56 +0100

    Add helper function to get bearer token from headers

---
+++ rest/jwt.go
@@ -0,0 +1,17 @@
+package rest
+
+import (
+	"net/http"
+	"strings"
+)
+
+// TokenFromHeader tries to retreive the token string from the
+// "Authorization" reqeust header: "Authorization: BEARER T".
+func TokenFromHeader(r *http.Request) string {
+	// Get token from authorization header.
+	bearer := r.Header.Get("Authorization")
+	if len(bearer) > 7 && strings.ToUpper(bearer[0:6]) == "BEARER" {
+		return bearer[7:]
+	}
+	return ""
+}
