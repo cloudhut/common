c27bf941beed5a8386fbbd5c5bf9d42df19637f4
rikimaru0345 <rikidev0345@gmail.com>
2020-01-09 16:16:45 +0100

    feature: extra header for access log: allow specifying the name of a header that will be included in the access logs for each request

--- middleware/access_log.go
+++ middleware/access_log.go
@@ -7,18 +7,19 @@ import (
 
 	"github.com/go-chi/chi/middleware"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 // AccessLog implements the middleware interface
 type AccessLog struct {
-	logger *zap.Logger
+	logger                *zap.Logger
+	extraHeader           string
+	blockWhenExtraMissing bool
 }
 
 // NewAccessLog creates a new middleware which prints access logs
-func NewAccessLog(logger *zap.Logger) *AccessLog {
-	return &AccessLog{
-		logger: logger,
-	}
+func NewAccessLog(logger *zap.Logger, extraHeader string, blockWhenExtraMissing bool) *AccessLog {
+	return &AccessLog{logger, extraHeader, blockWhenExtraMissing}
 }
 
 // Wrap implements the middleware interface
@@ -26,6 +27,36 @@ func (a *AccessLog) Wrap(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 
+		fields := []zapcore.Field{
+			zap.String("log_type", "access"),
+			zap.String("remote_address", r.RemoteAddr),
+			//zap.Int64("response_time", durationMs),  // Added below
+			zap.String("protocol", r.Proto),
+			zap.String("request_method", r.Method),
+			zap.String("query_string", r.URL.RawQuery),
+			//zap.String("status", strconv.Itoa(ww.Status())),  // Added below
+			zap.String("uri", r.URL.Path),
+			zap.String("server_name", r.URL.Host),
+			zap.Int64("bytes_received", r.ContentLength),
+			//zap.Int("bytes_sent", ww.BytesWritten()),  // Added below
+			zap.String("remote_client_id", r.Header.Get("remoteClientId")),
+		}
+
+		if len(a.extraHeader) > 0 {
+			v := r.Header.Get(a.extraHeader)
+			if len(v) > 0 {
+				// We found the wanted extra header
+				fields = append(fields, zap.String(a.extraHeader, v))
+			} else {
+				// Extra header is missing, maybe we should block the request
+				if a.blockWhenExtraMissing {
+					a.logger.Info("request blocked, required extra header missing", fields...)
+					return // dont even execute the request!
+				}
+			}
+		}
+
+		// Call next handler
 		next.ServeHTTP(w, r)
 
 		ww, ok := w.(middleware.WrapResponseWriter)
@@ -36,20 +67,13 @@ func (a *AccessLog) Wrap(next http.Handler) http.Handler {
 		duration := time.Since(start)
 		durationMs := duration.Nanoseconds() / (1000 * 1000)
 
-		a.logger.Info(
-			"",
-			zap.String("log_type", "access"),
-			zap.String("remote_address", r.RemoteAddr),
+		// Append fields that are only available after the request has completed
+		fields = append(fields,
 			zap.Int64("response_time", durationMs),
-			zap.String("protocol", r.Proto),
-			zap.String("request_method", r.Method),
-			zap.String("query_string", r.URL.RawQuery),
 			zap.String("status", strconv.Itoa(ww.Status())),
-			zap.String("uri", r.URL.Path),
-			zap.String("server_name", r.URL.Host),
-			zap.Int64("bytes_received", r.ContentLength),
 			zap.Int("bytes_sent", ww.BytesWritten()),
-			zap.String("remote_client_id", r.Header.Get("remoteClientId")),
 		)
+
+		a.logger.Info("", fields...)
 	})
 }
