77c6b243c55d126bfb593c533fdd6a84e890027b
Martin Schneppenheim <weeco91@gmail.com>
2020-12-19 13:43:25 +0100

    Return more detailed error messages in decode()

--- rest/decode.go
+++ rest/decode.go
@@ -2,23 +2,94 @@ package rest
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"github.com/cloudhut/common/header"
+	"io"
 	"net/http"
+	"strings"
 )
 
-// Decode decodes the request body into v and calls its OK() function to validate the object.
-func Decode(r *http.Request, v interface{}) error {
-	err := json.NewDecoder(r.Body).Decode(v)
-	if err != nil {
-		return fmt.Errorf("decoding json failed: %w", err)
+// Decode tries to decode the request body into dst and calls its OK() function to validate the object.
+// It returns an error if:
+// - the content-type does not contain "application/json"
+// - body is smaller than 1MB
+// - any unknown fields were set
+// - deserialization fails
+// - the OK() method returns an error
+func Decode(w http.ResponseWriter, r *http.Request, dst interface{}) error {
+	if r.Header.Get("Content-Type") != "" {
+		value, _ := header.ParseValueAndParams(r.Header, "Content-Type")
+		if value != "application/json" {
+			msg := "Content-Type header is not application/json"
+			return &malformedRequest{status: http.StatusUnsupportedMediaType, msg: msg}
+		}
 	}
-	if valid, ok := v.(interface {
+
+	r.Body = http.MaxBytesReader(w, r.Body, 1048576)
+
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	err := dec.Decode(&dst)
+	if err != nil {
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+
+		switch {
+		case errors.As(err, &syntaxError):
+			msg := fmt.Sprintf("Request body contains badly-formed JSON (at position %d)", syntaxError.Offset)
+			return &malformedRequest{status: http.StatusBadRequest, msg: msg}
+
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			msg := fmt.Sprintf("Request body contains badly-formed JSON")
+			return &malformedRequest{status: http.StatusBadRequest, msg: msg}
+
+		case errors.As(err, &unmarshalTypeError):
+			msg := fmt.Sprintf("Request body contains an invalid value for the %q field (at position %d)", unmarshalTypeError.Field, unmarshalTypeError.Offset)
+			return &malformedRequest{status: http.StatusBadRequest, msg: msg}
+
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+			msg := fmt.Sprintf("Request body contains unknown field %s", fieldName)
+			return &malformedRequest{status: http.StatusBadRequest, msg: msg}
+
+		case errors.Is(err, io.EOF):
+			msg := "Request body must not be empty"
+			return &malformedRequest{status: http.StatusBadRequest, msg: msg}
+
+		case err.Error() == "http: request body too large":
+			msg := "Request body must not be larger than 1MB"
+			return &malformedRequest{status: http.StatusRequestEntityTooLarge, msg: msg}
+
+		default:
+			return err
+		}
+	}
+
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		msg := "Request body must only contain a single JSON object"
+		return &malformedRequest{status: http.StatusBadRequest, msg: msg}
+	}
+
+	if valid, ok := dst.(interface {
 		OK() error
 	}); ok {
 		err = valid.OK()
 		if err != nil {
-			return fmt.Errorf("validating the decoded object failed: %w", err)
+			return &malformedRequest{status: http.StatusBadRequest, msg: fmt.Sprintf("validating the decoded object failed: %v", err.Error())}
 		}
 	}
+
 	return nil
+}
+
+type malformedRequest struct {
+	status int
+	msg    string
+}
+
+func (mr *malformedRequest) Error() string {
+	return mr.msg
 }
